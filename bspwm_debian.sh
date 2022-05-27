ddd861612c5803be895d8c01731387a8ec0da6b0
ShogoXY <80745742+ShogoXY@users.noreply.github.com>
2022-05-27 11:16:29 +0200

    Update bspwm_debian.sh

--- bspwm_debian.sh
+++ bspwm_debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo""
+echo ""
 echo "+++++++++++++++++++++++++++++++++++++++++"
 echo "+                                       +"
 echo "+     Instalation and simple config     +"
